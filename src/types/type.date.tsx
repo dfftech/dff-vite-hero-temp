b7157dfaae82bc271afef246cacd25e96080569f
sivakumarreddygurram <sivakumarreddy.gurram@gmail.com>
2025-06-06 18:48:48 +0530

    added-timings-for-type-date

--- src/types/type.date.tsx
+++ src/types/type.date.tsx
@@ -1,16 +1,22 @@
 import { DatePicker } from "@heroui/date-picker";
+import { now, getLocalTimeZone } from "@internationalized/date";
 
 interface DateProps {
   className?: string;
+  isDateTimeEnabled?: boolean;
 }
 
 const TypeDatePicker: React.FC<DateProps> = ({
   className,
+  isDateTimeEnabled = false,
 }: DateProps) => {
   return (
     <DatePicker
       className={className}
       radius="full"
+      hideTimeZone={!isDateTimeEnabled}
+      showMonthAndYearPickers
+      defaultValue={isDateTimeEnabled ? now(getLocalTimeZone()) : undefined}
     />
   );
 };
